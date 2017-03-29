29b68fb3d39ac58846b166ced4f9f574142458dd
allyjlevine <allyjlevine@gmail.com>
2017-03-28 21:20:24 -0400

    adding treetest

--- js/jquery.js
+++ js/jquery.js
@@ -4,4 +4,7 @@ $( document ).ready(function() {
 
 $(function(){
   $("#finder-content").load("finder/finder.html");
+  $("#sticky-note-content").load("sticky-note/index.html");
+  $("#finder-content-tablet").load("finder/finder.html");
+  $("#sticky-note-content-tablet").load("sticky-note/index.html");
 });
